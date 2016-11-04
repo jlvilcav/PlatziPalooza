400c325927ef2f4ca938b8ae5e7505352dad96b6
Jorge Vilca <jlvilcav@gmail.com>
2016-11-03 23:12:52 -0500

    Clase 03.11.2016 Menus con navs y js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation()
+
+$('[data-toggle-dia]').click(function (ev) {
+	/* Act on the event */
+ 	const panel = $(this).data('toggleDia')
+	$('#lineup-tabs').foundation('selectTab', panel)
+});
