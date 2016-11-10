1fa43345ad1ad6276725e35545a8f1d1f85f9f3c
Jorge Vilca <jlvilcav@gmail.com>
2016-11-10 00:42:23 -0500

    Final de clase Foundation 10.11.2016

--- js/app.js
+++ js/app.js
@@ -5,3 +5,15 @@ $('[data-toggle-dia]').click(function (ev) {
  	const panel = $(this).data('toggleDia')
 	$('#lineup-tabs').foundation('selectTab', panel)
 });
+
+
+const $offCanvas = $('#offCanvas');
+const $sticky = $('#sticky');
+
+$offCanvas.find('li').click(function(ev){
+	$offCanvas.foundation('close')
+	setTimeout(function () {
+		$sticky.css('left', '0px')
+	}, 300)
+	
+})
